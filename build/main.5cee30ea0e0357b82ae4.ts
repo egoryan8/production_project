47f6645f9c1a3144970cd90759af716985ac1cbc
Fogel Egor <fogel_92@bk.ru>
2022-09-16 16:31:06 +0700

    feat: add webpack config

---
+++ build/main.5cee30ea0e0357b82ae4.ts
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index.js):\\nError: Debug Failure. False expression: Non-string value passed to `ts.resolveTypeReferenceDirective`, likely by a wrapping package working with an outdated `resolveTypeReferenceDirectives` signature. This is probably not a problem in TS itself.\\n    at Object.resolveTypeReferenceDirective (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:43192:18)\\n    at H:\\\\dev\\\\production-project\\\\node_modules\\\\ts-loader\\\\dist\\\\servicesHost.js:713:18\\n    at H:\\\\dev\\\\production-project\\\\node_modules\\\\ts-loader\\\\dist\\\\servicesHost.js:128:141\\n    at Array.map (<anonymous>)\\n    at Object.resolveTypeReferenceDirectives (H:\\\\dev\\\\production-project\\\\node_modules\\\\ts-loader\\\\dist\\\\servicesHost.js:128:124)\\n    at actualResolveTypeReferenceDirectiveNamesWorker (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:118249:163)\\n    at resolveTypeReferenceDirectiveNamesWorker (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:118549:26)\\n    at processTypeReferenceDirectives (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:120046:31)\\n    at findSourceFileWorker (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:119931:21)\\n    at findSourceFile (H:\\\\dev\\\\production-project\\\\node_modules\\\\typescript\\\\lib\\\\typescript.js:119783:26)\");\n\n//# sourceURL=webpack://production-project/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    filename: '[name].[contenthash].ts',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+export default config;
